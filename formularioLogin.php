f07e06f736ee5c6f15fbf8a03a65e5bc61447026
Narbis <narbistamiche@gmail.com>
2019-09-29 11:57:42 -0300

    unifique estilos formularios

--- formularioLogin.php
+++ formularioLogin.php
@@ -7,16 +7,20 @@
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
     integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    
+    <link href="https://fonts.googleapis.com/css?family=Livvic|Nunito|Raleway&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Livvic|Nunito|Raleway&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Courgette|Dancing+Script&amp;display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="css/Login_Registrarse.css">
     <link rel="stylesheet" href="css/main.css">
 
     <title></title>
   </head>
 
-    <?php require("encabezado.php"); ?>
 
   <body>
+
+    <?php require("encabezado.php"); ?>
 
     <main>
       <div class="container">
@@ -25,8 +29,16 @@
 
           </div>
           <div class="col-12 col-lg-6">
+            <div class="tituloPrincipal">
+              <h1>Inicia Sesión</h1>
+              <h3>¡Mi cuenta!</h3>
+            </div>
+
             <form id="formulario">
-              <div class="form-group">
+                <p id="titulo-form"><b>Ingresa tus Datos</b></p>
+                <div class= "user_info">
+                <div class="form-group">
+
                 <label for="email">Email</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
               </div>
@@ -41,7 +53,10 @@
               <div class="registrarse">
                 <small id="pass">¿No tenés cuenta? <a href="formularioRegistro.php">Registrate</a> </small>
               </div>
+
             </form>
+          </div>
+
           </div>
           <div class="col-lg-3">
 
@@ -57,17 +72,9 @@
 
 
 
-
-
-
-
-
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
   </body>
 </html>
--- formularioRegistro.php
+++ formularioRegistro.php
@@ -36,79 +36,78 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="css/Login_Registrarse.css">
+    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Livvic|Nunito|Raleway&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Courgette|Dancing+Script&amp;display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/contacto.css">
     <title></title>
   </head>
   <body>
 
+      <?php require("encabezado.php"); ?>
 
-    <?php require("encabezado.php"); ?>
+      <main>
+        <div class="container">
+          <div class="row contenedor">
+            <div class="col-lg-3">
 
-    <main>
-      <div class="container">
-        <div class="row contenedor">
-          <div class="col-lg-3">
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="tituloPrincipal">
+                  <h1>Registrate</h1>
+                  <h3>¡Únete para hacer tus pedidos!</h3>
+                </div>
+              <form id="formulario" method='post'>
+                <p id="titulo-form"><b>Ingresa tus Datos</b></p>
+                <div class= "user_info">
+                <div class="form-group" >
+                  <label for="nombre">Nombre</label>
+                  <input type="text" class="form-control" name="nombre" value="">
+                </div>
+                <div class="form-group">
+                  <label for="tel">Telefono</label>
+                  <input type="tel" class="form-control" name="telefono" value="">
+                </div>
+                <div class= form-group>
+                  <label for="email">Email</label>
+                  <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" >
+                </div>
+                <div class="form-group">
+                  <label for="password">Contraseña</label>
+                  <input type="password" class="form-control" name="password" id="exampleInputPassword1">
+                </div>
+              <!--  <div class="form-group">
+                  <label for="password">Confirmar contraseña</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" >
+                </div>
+              -->
+                <div class="boton"> <!--es de arriba, revisar que sea la misma, videos DH -->
+                  <button type="submit" class="btn btn_login" >Registrarse</button>
+                </div>
+              </div>
+              </form>
+            </div>
+
+            <div class="col-lg-3">
+
+            </div>
+
 
           </div>
-          <div class="col-12 col-lg-6">
-            <form id="formulario" method='post'>
-              <div class="form-group" >
-                <label for="nombre">Nombre</label>
-                <input type="text" class="form-control" name="nombre" value="">
-              </div>
-              <div class= form-group>
-                <label for="email">Email</label>
-                <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" >
-              </div>
-              <div class="form-group">
-                <label for="tel">Telefono</label>
-                <input type="tel" class="form-control" name="telefono" value="">
-              </div>
-              <div class="form-group">
-                <label for="password">Contraseña</label>
-                <input type="password" class="form-control" name="password" id="exampleInputPassword1">
-              </div>
-            <!--  <div class="form-group">
-                <label for="password">Confirmar contraseña</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" >
-              </div>
-            -->
-              <div class="boton"> <!--es de arriba, revisar que sea la misma, videos DH -->
-                <button type="submit" class="btn btn_login" >Registrarse</button>
-              </div>
-            </form>
-          </div>
-
-          <div class="col-lg-3">
-
-          </div>
-
-
         </div>
-      </div>
 
 
-    </main>
+      </main>
 
 
+      <?php require("pieDePagina.php"); ?>
+
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+      </body>
 
 
-    <?php require("pieDePagina.php"); ?>
-
-
-
-
-
-
-
-   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-   crossorigin="anonymous"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-   integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-   integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
+      </html>
